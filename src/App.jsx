73430c01e75d2982e4a90f32ae94866c82ba60cd
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-07-30 18:54:02 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Quiz</h1>
+      <main>
+        <div id="informationsQuestion">
+          <span>Aqui estará a pergunta</span>
+          <span>1/4</span>
+        </div>
+        <ul>
+          <li>Opção</li>
+          <li>Opção</li>
+          <li>Opção</li>
+        </ul>
+      </main>
+    </div>
+  )
+}
+
+export default App
