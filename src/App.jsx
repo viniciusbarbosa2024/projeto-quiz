194f7167b5ab31769d3640c7f9aca3a7a4ae4c14
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-07-30 21:08:44 -0300

    Função nextQuestion
    Função para ir para a próxima pergunta após o usuário responder a pergunta atual

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,131 @@
 import React from 'react'
+import { useState } from 'react'
 import './App.css'
 
 const App = () => {
+  const [index,setIndex] = useState(1)
+  
+  const question = [
+    {
+    name: 'Quanto é 1+1?',
+    alternative: [
+      {
+        name: 5,
+        status: false
+      },
+
+      {
+        name: 2,
+        status: true
+      },
+
+      {
+        name: 6,
+        status: false
+      },
+
+    ]
+  },
+
+  {
+    name: 'Quanto é 10/2?',
+    alternative: [
+      {
+        name: 5,
+        status: true
+      },
+
+      {
+        name: 4,
+        status: false
+      },
+
+      {
+        name: 6,
+        status: false
+      },
+
+    ]
+  },
+
+  {
+    name: 'Quanto é 6 x 6?',
+    alternative: [
+      {
+        name: 36,
+        status: true
+      },
+
+      {
+        name: 64,
+        status: false
+      },
+
+      {
+        name: 42,
+        status: false
+      },
+
+    ]
+  },
+
+  {
+    name: 'Quanto é 8^2?',
+    alternative: [
+      {
+        name: 81,
+        status: false
+      },
+
+      {
+        name: 49,
+        status: false
+      },
+
+      {
+        name: 64,
+        status: true
+      },
+
+    ]
+  },
+
+
+  ]
+
+  function nextQuestion() {
+    setIndex(index+1)   
+  }
+
+
+  
   return (
     <div>
       <h1>Quiz</h1>
       <main>
         <div id="informationsQuestion">
-          <span>Aqui estará a pergunta</span>
-          <span>1/4</span>
+          <span>{question[index-1].name}</span>
+          <span>{index}/4</span>
         </div>
         <ul>
-          <li>Opção</li>
-          <li>Opção</li>
-          <li>Opção</li>
+          <li onClick={nextQuestion}>
+
+            {question[index-1].alternative[0].name}
+
+          </li>
+
+          <li onClick={nextQuestion}>
+
+            {question[index-1].alternative[1].name}
+
+          </li>
+
+          <li onClick={nextQuestion}>
+
+            {question[index-1].alternative[2].name}
+
+          </li>
+
         </ul>
       </main>
     </div>
