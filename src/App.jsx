a1134105dba62dd215cb30a0f404891f8f17e2c4
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-08-01 13:16:12 -0300

    Coloquei as questions num arquivo externo

--- src/App.jsx
+++ src/App.jsx
@@ -2,99 +2,13 @@ import React from 'react'
 import { useState } from 'react'
 import './App.css'
 import ResultHTML from "./components/ResultHTML.jsx"
+import question from './components/Questions.js'
 
 const App = () => {
   const [index,setIndex] = useState(1)
 
   const [alternativeStatus,setAlternativeStatus] = useState([])
   
-  const question = [
-    {
-    name: 'Quanto é 1+1?',
-    alternative: [
-      {
-        name: 5,
-        status: false
-      },
-
-      {
-        name: 2,
-        status: true
-      },
-
-      {
-        name: 6,
-        status: false
-      },
-
-    ]
-  },
-
-  {
-    name: 'Quanto é 10/2?',
-    alternative: [
-      {
-        name: 5,
-        status: true
-      },
-
-      {
-        name: 4,
-        status: false
-      },
-
-      {
-        name: 6,
-        status: false
-      },
-
-    ]
-  },
-
-  {
-    name: 'Quanto é 6 x 6?',
-    alternative: [
-      {
-        name: 36,
-        status: true
-      },
-
-      {
-        name: 64,
-        status: false
-      },
-
-      {
-        name: 42,
-        status: false
-      },
-
-    ]
-  },
-
-  {
-    name: 'Quanto é 8^2?',
-    alternative: [
-      {
-        name: 81,
-        status: false
-      },
-
-      {
-        name: 49,
-        status: false
-      },
-
-      {
-        name: 64,
-        status: true
-      },
-
-    ]
-  },
-
-
-  ]
 
   function nextQuestion() {
     setIndex(index+1)  
